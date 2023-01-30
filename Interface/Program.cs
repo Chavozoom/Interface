a6eb7ed558de136b86c7e0401830dffb6fcb5642
Chavozoom <isaquechaves10500@gmail.com>
2023-01-29 22:19:16 -0300

    F Project

---
+++ Interface/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using Interface.Entities;
+using Interface.Services;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Date(dd/MM/yyyy): ");
+            DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+
+            double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter number of installments: ");
+            int installments = int.Parse(Console.ReadLine());
+
+            Contract contract = new Contract(number,date,contractValue);
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(contract, installments);
+
+            Console.WriteLine("INSTALLMENTS:");
+            foreach (Installment installment in contract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+            Console.WriteLine();
+        }
+    }
+}
